7a4e69ef6c639e29c103dd2f03fed94e59b088bc
Rodrigo Galdino <63233782+rodrigogaldino553@users.noreply.github.com>
2020-08-06 00:55:10 -0300

    terceiro dia finalizado

---
+++ scripts/addField.js
@@ -0,0 +1,20 @@
+document.querySelector("#add-time")
+
+.addEventListener('click', cloneField)
+
+
+function cloneField(){
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    const fields = newFieldContainer.querySelectorAll('input')
+    
+    fields.forEach(function(field) {
+        field.value = ""
+    });
+    fields[0].value = ""
+    fields[1].value = ""
+    
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
+
+
