5033e5cdf20508555b8aa5ba20848a93d789c718
Rodrigo Galdino <63233782+rodrigogaldino553@users.noreply.github.com>
2020-08-08 14:39:40 -0300

    ultimo dia, funcionando

--- src/database/test.js
+++ src/database/test.js
@@ -2,11 +2,13 @@ const database = require('./db')
 
 const createProffy = require('./createProffy')
 
+
+
 database.then(async(db) => {
 //inserir dados
 
     proffyValue = {
-        name:"Felipe Montano",
+        name:"Lucas Montano",
         avatar: "https://avatars2.githubusercontent.com/u/7559318?s=460&u=9b45fc98ab22e71efa4951eee14cf349a73411ec&v=4",
         whatsapp: "19348554567",
         bio: "Muita experiencia com busca binária com kotlin no android",
@@ -14,7 +16,7 @@ database.then(async(db) => {
     }
 
     classValue = {
-        subject: 'Artes',
+        subject: 1,
         cost: '40',
         //o id vai vir pelo banco de dados
     }
@@ -40,7 +42,30 @@ database.then(async(db) => {
 
     //todos os proffys
     const selectedProffys = await db.all("SELECT * FROM proffys")
-    console.log(selectedProffys)
+    //console.log(selectedProffys)
+
+    //consultar as classes de um determinado proffy e trazer os dados tbm
+    const selectedClassesAllProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id == proffy_id)
+        WHERE classes.proffy_id == 1;
+    `)
+
+    //console.log(selectedClassesAllProffys)
+
+    //pegar pelo horario
+
+    const selectedClassesSchedule = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id == 1
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1330"
+        AND class_schedule.time_to > "1330"
+    `)
+
+    //console.log(selectedClassesSchedule)
 })
 
 
---
+++ src/utils/format.js
@@ -0,0 +1,34 @@
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química"
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+
+
+function getSubject(subjectNumber){
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
+module.exports = {
+    subjects,
+    weekdays,
+    getSubject
+}
